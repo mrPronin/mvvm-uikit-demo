dc1954df546187c34f7a1f19f4fa4b3d4b63015c
Oleksandr Pronin <pronin.alx@gmail.com>
2022-12-04 15:38:20 +0100

    feat: update user list view controller boilerplate

--- mbition-ios/Scene/UserList/UserList+ViewController.swift
+++ mbition-ios/Scene/UserList/UserList+ViewController.swift
@@ -10,6 +10,54 @@ import Combine
 
 extension UserList {
     class ViewController: UIViewController {
+        // MARK: - Init
+        init(viewModel: UserListViewModel) {
+            self.viewModel = viewModel
+            super.init(nibName: nil, bundle: nil)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        // MARK: - View Lifecycle
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupOnLoad()
+            bindViewModel()
+        }
+
+        // MARK: - UI
+        
+        // MARK: - Private
+        let viewModel: UserListViewModel
+        var router: UserListRouter!
+        let load = PassthroughSubject<Void, Never>()
+        var subscriptions = Set<AnyCancellable>()
+    }
+}
+
+// MARK: - Private
+extension UserList.ViewController {
+    @objc private func handleRefresh(_ sender: UIRefreshControl) {
+        // TODO: Update content
+//        load.send(.init(sorting: viewModel.sorting, scope: .network))
+        
+        DispatchQueue.main.async {
+            sender.endRefreshing()
+        }
+    }
+    
+    private func bindActions(with output: UserList.ViewModel.Output) {
+        // TODO: implement bind actions
+    }
+    
+    private func bindViewModel() {
+        // TODO: implement bind view model
+    }
+    
+    private func setupOnLoad() {
         
     }
+
 }
