be64de74e2634fa1360fb6b1a8dba67d55566fcc
Oleksandr Pronin <pronin.alx@gmail.com>
2022-12-04 14:49:03 +0100

    feat: add user details view mode boilerplate

--- mbition-ios/ViewModel/UserDetails+ViewModel.swift
+++ mbition-ios/ViewModel/UserDetails+ViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import Combine
+
+// Define view model protocol
+protocol UserDetailsViewModel {
+    func transform(input: UserDetails.ViewModel.Input) -> UserDetails.ViewModel.Output
+}
+
+// Extend namespace
+extension UserDetails { enum ViewModel {} }
+
+// Define input / output
+extension UserDetails.ViewModel {
+    struct Input {
+        let load: AnyPublisher<Void, Never>
+    }
+    struct Output {
+        let userList: AnyPublisher<[UserDetails.Model], Never>
+        let error: AnyPublisher<Error, Never>
+        let activityIndicator: AnyPublisher<Bool, Never>
+        let loadingBanner: AnyPublisher<Bool, Never>
+    }
+}
+
+// Implementation
+extension UserDetails.ViewModel {
+    struct Implementation: UserDetailsViewModel {
+        func transform(input: UserDetails.ViewModel.Input) -> UserDetails.ViewModel.Output {
+            return UserDetails.ViewModel.Output(
+                userList: Empty().eraseToAnyPublisher(),
+                error: Empty().eraseToAnyPublisher(),
+                activityIndicator: Empty().eraseToAnyPublisher(),
+                loadingBanner: Empty().eraseToAnyPublisher()
+            )
+        }
+        
+        // MARK: - Init
+        // MARK: - Logic
+        // MARK: - Dependency
+        // MARK: - Private
+    }
+}
+
+
