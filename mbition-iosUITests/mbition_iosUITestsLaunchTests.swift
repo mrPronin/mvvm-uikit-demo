dc1b97646cce53e7dc78766f283c83062cd53b5b
Oleksandr Pronin <pronin.alx@gmail.com>
2022-12-04 12:22:58 +0100

    Initial Commit

---
+++ mbition-iosUITests/mbition_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mbition_iosUITestsLaunchTests.swift
+//  mbition-iosUITests
+//
+//  Created by Pronin Oleksandr on 04.12.22.
+//
+
+import XCTest
+
+final class mbition_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
