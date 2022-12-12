e4cdda498f9e3d794cdbf33c1134d909a356b730
Oleksandr Pronin <pronin.alx@gmail.com>
2022-12-12 22:21:45 +0100

    chore: clean-up

--- mbition-ios/Scene/UserDetails/UserDetails+Assembly.swift
+++ mbition-ios/Scene/UserDetails/UserDetails+Assembly.swift
@@ -9,28 +9,6 @@ import UIKit
 
 extension UserDetails {
     static func viewController(with userListModel: UserList.Model) -> UIViewController {
-        // debug
-        /*
-        // mock
-        let testBundle = Bundle(for: UserDetails.ViewController.self)
-        let path = testBundle.path(forResource: "user-details", ofType: "json")!
-        let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
-        var userDetails: UserDetails.Model?
-        do {
-            userDetails = try JSONDecoder().decode(UserDetails.Model.self, from: data!)
-        } catch {
-            LOG(error)
-        }
-//        LOG(userDetails)
-        // mock
-        
-        let userDetailsService = UserDetails.Service.Mock()
-//        userDetailsService.error = Network.Errors.notFound
-        userDetailsService.userDetails = userDetails
-        
-        */
-        // debug
-        
         let userDetailsService = UserDetails.Service.Implementation()
         let viewModel = UserDetails.ViewModel.Implementation(
             userDetailsService: userDetailsService,
--- mbition-ios/Scene/UserList/UserList+Assembly.swift
+++ mbition-ios/Scene/UserList/UserList+Assembly.swift
@@ -9,27 +9,6 @@ import UIKit
 
 extension UserList {
     static var vc: UIViewController {
-        // debug
-        /*
-        // mock
-        let testBundle = Bundle(for: UserList.ViewController.self)
-        let path = testBundle.path(forResource: "user-list", ofType: "json")!
-        let data = try? Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
-        var userList: [UserList.Model] = []
-        do {
-            userList = try JSONDecoder().decode([UserList.Model].self, from: data!)
-        } catch {
-            LOG(error)
-        }
-//        LOG(userList)
-        // mock
-        
-        let userListService = UserList.Service.Mock()
-//        userListService.error = Network.Errors.notFound
-        userListService.userListArray = userList
-        */
-        // debug
-        
         let userListService = UserList.Service.Implementation()
         let viewModel = UserList.ViewModel.Implementation(userListService: userListService)
         
